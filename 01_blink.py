008fcffaa258d6de80fbc789cfee43709576f6e8
simonmonk <srmonk@gmail.com>
2019-09-25 03:20:31 -0900

    tidying

--- 01_blink.py
+++ 01_blink.py
@@ -1,7 +1,6 @@
 # 01_blink.py
 # From the code for the Electronics Starter Kit for the Raspberry Pi by MonkMakes.com
 
-
 from gpiozero import LED
 import time
 from signal import pause
@@ -11,10 +10,6 @@ red_led = LED(18)
 # The first number in blink() is the on time and the second is the off time (both in seconds)
 red_led.blink(on_time=0.5, off_time=0.5)
 
-pause()
+pause()s
 
-'''
-The pause() is needed at the end to keep the program running.
-If you remove it, the program will stop immediately and the LED's will stop blinking.
-Use CTRL-c to stop the program running.
-'''
+# The pause() is needed at the end to keep the program running.
