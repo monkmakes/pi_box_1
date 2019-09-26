0552239de3bda74df9d883079a62960537331bb4
simonmonk <srmonk@gmail.com>
2019-09-26 04:12:14 -0900

    improved proj 7

--- 07_reactions.py
+++ 07_reactions.py
@@ -1,37 +1,39 @@
 # 06_reactions.py
 # From the code for the Electronics Starter Kit for the Raspberry Pi by MonkMakes.com
 
-from gpiozero import RGBLED, Button
+from gpiozero import LED, Button
 import time, random
 
-rgb_led = RGBLED(red=18,green=23, blue=17) # Pin 17 is not connected but RGBLED() requires 3 pins
-red_switch = Button(25)
-green_switch = Button(24)
+left_led = LED(25)
+right_led = LED(23)
+left_switch = Button(24)
+right_switch = Button(18)
 
-# find which buttons pressed 0 means neither, -1=both, 2=red, 1=green 
+# find which buttons pressed 0 means neither, -1=both, 2=right, 1=left
 def key_pressed():
     # if button is pressed is_pressed will report false for that input
-    if red_switch.is_pressed and green_switch.is_pressed:
+    if left_switch.is_pressed and right_switch.is_pressed:
         return -1
-    if not red_switch.is_pressed and not green_switch.is_pressed:
+    if not left_switch.is_pressed and not right_switch.is_pressed:
         return 0
-    if not red_switch.is_pressed and green_switch.is_pressed:
+    if not right_switch.is_pressed and left_switch.is_pressed:
         return 1
-    if red_switch.is_pressed and not green_switch.is_pressed:
+    if right_switch.is_pressed and not left_switch.is_pressed:
         return 2
 
 while True:
-    rgb_led.off()
-    print("Press the button for red or green when one lights")
+    left_led.off()
+    right_led.off()
+    print("Press the button next to the LED that lights up")
     delay = random.randint(3, 7)    # random delay of 3 to 7 seconds
     color = random.randint(1, 2)    # random color red=1, green=2
     time.sleep(delay)
-    if (color == 2):
-        print("red")
-        rgb_led.red = 1
+    if (color == 1):
+        print("left")
+        left_led.on()
     else:
-        print("green")
-        rgb_led.green = 1
+        print("right")
+        right_led.on()
     t1 = time.time()
     while not key_pressed():
         pass
