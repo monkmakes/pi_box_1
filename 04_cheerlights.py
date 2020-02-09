cd09c8592cc775c80143239c6fca154653c9cf85
simonmonk <evilgeniusauthor@gmail.com>
2020-02-09 08:49:43 +0100

    changed cheerlights

--- 04_cheerlights.py
+++ 04_cheerlights.py
@@ -10,16 +10,16 @@ cheerlights_url = "http://api.thingspeak.com/channels/1417/field/2/last.txt"
 old_color = None
 
 def pressed():
-    led.color = Color(0, 0, 0)
+    led.color = Color(0, 0, 0)  # LED off
 button.when_pressed = pressed
 
 while True:
     try:
         cheerlights = requests.get(cheerlights_url)
-        color = cheerlights.content
+        color = cheerlights.content     # the color as text
         if color != old_color:
-            led.color = Color(color)
-            old_color = color
+            led.color = Color(color)    # the color as an object
+            old_color = color           
     except Exception as e:
         print(e)
-    time.sleep(update_period)
+    time.sleep(update_period)  # don't flood the web service
